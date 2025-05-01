be265d095aeea83cbc9d025979acc1ecafa65905
snehal <snehalvanage57@gmail.com>
2025-05-01 12:51:02 +0530

    do routing on django project

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hey this is home page")
+
+def about(request):
+    return HttpResponse("Hey this is about page")
